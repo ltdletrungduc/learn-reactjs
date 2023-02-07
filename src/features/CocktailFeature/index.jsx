e66fb810d542326d09fca806dfc7358e5f52cfcb
Le Trung Duc <ltdletrungduc@gmail.com>
2023-02-07 12:16:59 +0700

    [upd] prettier config, add axios

--- src/features/CocktailFeature/index.jsx
+++ src/features/CocktailFeature/index.jsx
@@ -2,27 +2,33 @@ import React, { useEffect, useState } from 'react';
 import CocktailList from './components/CockTailList';
 
 function CocktailFeature() {
-	const [cocktailList, setCocktailList] = useState([]);
+  const [cocktailList, setCocktailList] = useState([]);
 
-	useEffect(() => {
-		fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
-			.then((response) => response.json())
-			.then((data) => setCocktailList(data.drinks))
-			.catch((err) => console.error(err));
-	}, []);
-	return (
-		<div className='cocktail-feature'>
-			<h2
-				style={{
-					fontSize: '30px',
-					fontWeight: 'bold',
-					marginBottom: '30px',
-				}}>
-				COCKTAIL FEATURE
-			</h2>
-			<CocktailList list={cocktailList} />
-		</div>
-	);
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic', { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => setCocktailList(data.drinks))
+      .catch((err) => console.error(err));
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  return (
+    <div className="cocktail-feature">
+      <h2
+        style={{
+          fontSize: '30px',
+          fontWeight: 'bold',
+          marginBottom: '30px',
+        }}
+      >
+        COCKTAIL FEATURE
+      </h2>
+      <CocktailList list={cocktailList} />
+    </div>
+  );
 }
 
 export default CocktailFeature;
